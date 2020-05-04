d88b8be1d03c3328a55358a762e303369e03d9fd
jaelolpark <ac2cic@gmail.com>
2020-05-04 19:44:21 -0400

    first java git

---
+++ Begin/Exercise/Ch.1/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+//public: access control
+//main = function
+//void = no return value(int, str)
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
