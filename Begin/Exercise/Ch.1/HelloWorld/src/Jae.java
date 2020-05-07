d62a5701f5ae17634ce9fced5a3d8ce05da5d809
jaelolpark <ac2cic@gmail.com>
2020-05-07 01:22:13 -0400

    variables

---
+++ Begin/Exercise/Ch.1/HelloWorld/src/Jae.java
@@ -0,0 +1,13 @@
+public class Jae {
+    public static void main(String[] args) {
+        String name = "Jae";
+        int age = 31;
+        boolean married = true;
+
+        System.out.println("I am " + name);
+        System.out.println("I am " + age + " years old.");
+        System.out.println("Married: " + married);
+    }
+}
+
+
--- Begin/Exercise/Ch.1/HelloWorld/src/Variable.java
+++ Begin/Exercise/Ch.1/HelloWorld/src/Variable.java
@@ -1,8 +1,22 @@
-public class Variable {
-    public static void main(String[] args) {
-        boolean myBoolean = true;
-        myBoolean = false;
+//public class Variable {
+//    public static void main(String[] args) {
+//        boolean myBoolean = true;
+//        myBoolean = false;
+//
+//        System.out.println(myBoolean);
+//    }
+//}
 
-        System.out.println(myBoolean);
+public class Variable {
+    public static void main (String[] args) {
+        int myInt = 233;
+
+        long myLong = 1231241243L;
+
+        double myDouble = 3.14;
+
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myDouble);
     }
 }
