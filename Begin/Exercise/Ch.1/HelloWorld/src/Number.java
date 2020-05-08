a986e682f92905c003ec62bb08e71a68cb835142
jaelolpark <ac2cic@gmail.com>
2020-05-07 22:07:03 -0400

    number exercise

--- Begin/Exercise/Ch.1/HelloWorld/src/Number.java
+++ Begin/Exercise/Ch.1/HelloWorld/src/Number.java
@@ -1,6 +1,7 @@
 public class Number {
     public static void main(String[] args) {
         int a = 22;
+        int c = 8;
         double b = 2.0;
         System.out.println(a / b);
         System.out.println(9 / 5);
@@ -11,5 +12,17 @@ public class Number {
         System.out.println(true && true);
         System.out.println(true || false);
         System.out.println(!(false && (true || false)));
+        System.out.println((double) a / c );
+
+
+
+        int koreanScore = 86;
+        int mathScore = 94;
+        int scienceScore = 87;
+        int computerScore = 100;
+
+        System.out.println("Average: " + (double)(koreanScore + mathScore +scienceScore + computerScore) / 4);
+
+
     }
 }
