dac07b39d488c28f54e70c7bd26458128a050b97
jaelolpark <ac2cic@gmail.com>
2020-05-07 20:30:19 -0400

    number

---
+++ Begin/Exercise/Ch.1/HelloWorld/src/Number.java
@@ -0,0 +1,15 @@
+public class Number {
+    public static void main(String[] args) {
+        int a = 22;
+        double b = 2.0;
+        System.out.println(a / b);
+        System.out.println(9 / 5);
+        System.out.println(9.0 / 5);
+        System.out.println(291 > -20);
+        System.out.println(3 <= 3);
+        System.out.println(1 != 1);
+        System.out.println(true && true);
+        System.out.println(true || false);
+        System.out.println(!(false && (true || false)));
+    }
+}
