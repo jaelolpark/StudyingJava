704fb439fb5cc977931c5bff8d4d03fff57d9b31
jaelolpark <ac2cic@gmail.com>
2020-05-09 23:46:59 -0400

    switch

---
+++ Begin/Exercise/Ch.1/HelloWorld/src/Switch.java
@@ -0,0 +1,29 @@
+public class Switch {
+    public static void main(String[] args) {
+        int score = 98;
+
+        String grade;
+        switch (score / 10) {
+            case 10:
+                grade = "A+";
+                break;
+            case 9:
+                grade = "A";
+                break;
+            case 8:
+                grade = "B";
+                break;
+            case 7:
+                grade = "C";
+                break;
+            case 6:
+                grade = "D";
+                break;
+            default:
+                grade = "F";
+                break;
+
+        }
+        System.out.println(grade);
+    }
+}
